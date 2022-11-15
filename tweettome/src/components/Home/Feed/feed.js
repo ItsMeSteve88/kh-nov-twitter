7ddf3dcc3995e98dd3dfc5574181cb5df5c0691a
Steve <itsmesteve88@yahoo.com>
2022-11-15 13:02:52 +0000

    main feed added

--- tweettome/src/components/Home/Feed/feed.js
+++ tweettome/src/components/Home/Feed/feed.js
@@ -1,8 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
 import "./feed.css";
+import Trend from "../Trend/trend";
 
 function Feed() {
   return (
     <div className="feed-main">
+      {/* Search Box */}
       <div className="search-box">
         <div className="search-wrapper">
           <svg
@@ -17,7 +21,7 @@ function Feed() {
           </svg>
           <input placeholder="Search Twitter" type="search" />
         </div>
-        <svg 
+        <svg
           xmlns="http://www.w3.org/2000/svg"
           width="20"
           height="20"
@@ -29,6 +33,31 @@ function Feed() {
           <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
         </svg>
       </div>
+      {/* feed body */}
+      <nav>
+        <ul className="menus">
+          <li><a href="#"></a>For you</li>
+          <li><a href="#"></a>Trending</li>
+          <li><a href="#"></a>News</li>
+          <li><a href="#"></a>Sports</li>
+          <li><a href="#"></a>Entertainment</li>
+        </ul>
+      </nav>
+      
+      {/* banner */}
+      <div className="banner">
+        <p className="tag">War in Ukraine. LIVE</p>
+        <p className="text">Latest updates on the war in Ukraine</p>
+      </div>
+      {/* Trends */}
+      <Trend/>
+      <Trend/>
+      <Trend/>
+      <Trend/>
+      <Trend/>
+      <Trend/>
+      <Trend/>
+
     </div>
   );
 }
---
+++ tweettome/src/components/Home/Trend/trend.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import "./trend.css"
+
+// rfce
+function trend() {
+    return (
+        <div className='trend'>
+            <p className="header">Sports. Trending</p>
+            <p className="trend-text">#WorldCup22</p>
+            <p className="reTweets">1,100 Tweets</p>
+        </div>
+    );
+}
+
+export default trend;
